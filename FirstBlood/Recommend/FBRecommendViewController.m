8481d5e1027e9fee467c781de823e111bbf2318f
linjiaqiao <649207624@qq.com>
2016-04-19 17:47:14 +0800

    LJQ V2

--- FirstBlood/Recommend/FBRecommendViewController.m
+++ FirstBlood/Recommend/FBRecommendViewController.m
@@ -24,9 +24,8 @@
 //    [self setNavBar];
     
     [self setNavBarWithAlpha:.5 barTintColor:[UIColor whiteColor] tintColor:[UIColor yellowColor]];
+
     
-    NSLog(@"LJQ V1");
-    NSLog(@"LZJ V1");
 }
 
 #pragma mark - private methods
